c259c8f9623ac82dcccb1ff4bf16a111bdb257dd
jungtaek kim <kgajungtaek@gmail.com>
2024-02-25 11:02:57 +0900

    feat : 플레이어 구현
    플랫포머 캐릭터에 대한 움직임을 구현
    좌우 움직임 : 가속 - 최대속도 - 감속
    상하 움직임 : 점프 - 하강 - 최대하강

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Component")]
+    [SerializeField] new Rigidbody2D rigidbody;
+
+    [Header("Spec")]
+    [SerializeField] float maxSpeed;
+    [SerializeField] float accelPower;
+    [SerializeField] float decelPower;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float maxFallSpeed;
+
+    private Vector2 inputDir;
+
+    private void FixedUpdate()
+    {
+        Move();
+        Fall();
+    }
+
+    private void Move()
+    {
+        // Accelation
+        if (inputDir.x > 0 && rigidbody.velocity.x < maxSpeed)
+        {
+            rigidbody.AddForce(Vector2.right * inputDir.x * accelPower);
+        }
+        else if (inputDir.x < 0 && rigidbody.velocity.x > -maxSpeed)
+        {
+            rigidbody.AddForce(Vector2.right * inputDir.x * accelPower);
+        }
+
+        // Top Speed
+
+        // Deceleration
+        if (inputDir.x == 0 && rigidbody.velocity.x > 0.1f)
+        {
+            rigidbody.AddForce(Vector2.left * decelPower);
+        }
+        else if (inputDir.x == 0 && rigidbody.velocity.x < -0.1f)
+        {
+            rigidbody.AddForce(Vector2.right * decelPower);
+        }
+    }
+
+    private void Jump()
+    {
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpSpeed);
+    }
+
+    private void Fall()
+    {
+        if (rigidbody.velocity.y < maxFallSpeed)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, maxFallSpeed);
+        }
+    }
+
+    private void OnMove(InputValue value)
+    {
+        inputDir = value.Get<Vector2>();
+    }
+
+    private void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            Jump();
+        }
+    }
+}
